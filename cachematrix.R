adc16ab3c80f994e957e5a89445ea95bc1606900
S Kumar Pun <i67056@d2hs.com>
2016-05-05 14:13:35 +0545

    commited for coursera R programming -Assignment#2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation.
+# This is an example of caching the inverse of a matrix rather than computing it repeatedly. 
+# The following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing a function to
+## 1. set and get the value of the matrix
+## 2. set and get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set=set, get=get, 
+		setinverse=setinverse, 
+		getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of the matrix.
+## Checks if the inverse has already been computed. 
+## If yes, get the result and skips the computation. 
+## If no, compute the inverse, sets the value in the cache via setinverse function.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {    
+	i <- x$getinverse()
+    if(!is.null(i)) {
+        message("Retrieving matrix from cache")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinverse(i)
+    i
 }
+## Sample run:
+## > x = rbind(c(1, -1/4), c(-1/4, 1))
+## > m = makeCacheMatrix(x)
+## > m$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(m)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving matrix from cache in the second run
+## > cacheSolve(m)
+## Retrieving matrix from cache
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
